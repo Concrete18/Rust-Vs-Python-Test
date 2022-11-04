78174e5a4fb28d443aa7106971399f948ad601bb
Concrete18 <michaelericson19@gmail.com>
2022-11-04 05:03:19 -0400

    updated demo

--- Versus.py
+++ Versus.py
@@ -1,37 +1,49 @@
 from time import perf_counter
-
-
-def benchmark(func):
-    """
-    Prints `func` name and its benchmark time.
-    """
-
-    def wrapped(*args, **kwargs):
-        start = perf_counter()
-        value = func(*args, **kwargs)
-        end = perf_counter()
-        elapsed = round(end - start, 2)
-        print(f"{func.__name__} Completion Time: {elapsed}")
-        return value
-
-    return wrapped
+import rust_vs_python_test
 
 
 class Python:
-    @benchmark
-    def count(self):
+    def count(self, num):
+        print(f"\nPython count to {num:,}")
+        # start timer
+        start = perf_counter()
+        # start count
         n = 0
-        while n < 1_000_000_000_000:
+        while n < num:
             n += 1
+        # end timer
+        end = perf_counter()
+        elapsed = round((end - start) * 1000, 2)
+        print(f"Completion Time: {elapsed} ms")
+        return elapsed
 
 
 class Rust:
-    @benchmark
-    def count():
-        n = 0
-        while n < 1_000_000_000_000:
-            n += 1
+    def count(self, num):
+        print(f"\nRust count to {num:,}")
+        # start timer
+        start = perf_counter()
+        # start count
+        rust_vs_python_test.count(num)
+        # end timer
+        end = perf_counter()
+        elapsed = round((end - start) * 1000, 2)
+        print(f"Completion Time: {elapsed} ms")
+        return elapsed
 
 
-python = Python()
-python.count()
+def test():
+    python = Python()
+    rust = Rust()
+
+    num = 1_000_000_000_000
+
+    python_time = python.count(num)
+    rust_time = rust.count(num)
+
+    diff = round(python_time / rust_time, 1)
+    print(f"\nRust ran {diff} times faster then Python")
+
+
+if __name__ == "__main__":
+    test()
