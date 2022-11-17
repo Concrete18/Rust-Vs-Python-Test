32c65dd2c5a706e4fc40cf81cb7edc5262aebc4d
Concrete18 <michaelericson19@gmail.com>
2022-11-16 21:44:57 -0500

    finished speed testing code

--- Versus.py
+++ Versus.py
@@ -4,6 +4,9 @@ import rust_vs_python_test
 
 class Python:
     def count(self, num):
+        """
+        Counts to `num` from 0.
+        """
         # start timer
         start = perf_counter()
         # start count
@@ -17,6 +20,9 @@ class Python:
 
 class Rust:
     def count(self, num, run_count=1):
+        """
+        Counts to `num` from 0 using the imported Rust Library.
+        """
         # start timer
         start = perf_counter()
         # start count
@@ -34,17 +40,32 @@ class SpeedTest:
     rust = Rust()
 
     @staticmethod
-    def convert_times(elapsed_time):
+    def convert_time(elapsed_time):
+        """
+        Converts output from perf_counter into miliseconds and microseconds.
+        """
         ms = round((elapsed_time) * 1000, 2)
         us = round((elapsed_time) * 1000 * 1000, 2)
         return ms, us
 
     @staticmethod
-    def get_factor(slower_time, faster_time):
+    def get_factor(low_num, high_num):
         """
-        ph
+        Gets the factor of a `low_num` and a `high_num`.
         """
-        return round(slower_time / faster_time, 1)
+        return round(low_num / high_num, 1)
+
+    def run_test(self, num, rust_run_count=1):
+        """
+        Runs tests for Python and Rust. Rust runs a number of times equal to `rust_run_count`.
+        """
+        # runs each test
+        python_time = self.python.count(num)
+        python_ms, python_us = self.convert_time(python_time)
+
+        rust_time = self.rust.count(num, rust_run_count)
+        rust_ms, rust_us = self.convert_time(rust_time)
+        return python_ms, python_us, rust_ms, rust_us
 
     def count(self):
         """
@@ -54,13 +75,9 @@ class SpeedTest:
         NUM = 1_000_000
         print(f"\nTesting Count to {NUM:,}")
 
-        # runs each test
-        python_time = self.python.count(NUM)
-        python_ms, python_us = self.convert_times(python_time)
-
-        rust_time = self.rust.count(NUM, 10)
-        rust_ms, rust_us = self.convert_times(rust_time)
-
+        rust_run_count = 10
+        python_ms, python_us, rust_ms, rust_us = self.run_test(NUM, rust_run_count)
+        # sets up return values
         sleep(3)
 
         # prints times
@@ -72,23 +89,25 @@ class SpeedTest:
 
         # get differenct
         input("\nPress enter to show Rust Speed Factor")
-        factor = self.get_factor(python_time, rust_time)
+        factor = self.get_factor(python_ms, rust_ms)
         print(f"\nRust ran {factor} times faster then Python")
-
         input("\nPress Enter to Continue")
+        # retest code
+        print(f"Rust actually ran {rust_run_count} times while Python only ran once.")
 
-        print("\nRedoing Rust Speed Test")
-        rust_time = self.rust.count(NUM)
-        factor = self.get_factor(python_time, rust_time)
-        print(f"Rust actually ran {factor} times faster then Python")
+        input("\nPress enter to rerun Speed Tests.")
+        python_ms, python_us, rust_ms, rust_us = self.run_test(NUM)
+        print(f"Rust's Actual Completion Time: {rust_ms} ms or {rust_us} μs")
+        factor = self.get_factor(python_ms, rust_ms)
+        print(f"\nRust actually ran {factor} times faster then Python")
 
 
 def main():
-    print("\n\nRust VS Python Speed Test")
+    print("\nRust VS Python Speed Test")
     test = SpeedTest()
-    input("\nPress Enter to start first test")
+    input("\nPress Enter to start test.")
     test.count()
-    print("\n\n")
+    print("\n")
 
 
 if __name__ == "__main__":
